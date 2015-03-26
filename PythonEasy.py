e8b76855ac92c3dfacfbe06a9f470753719641fd
Blake Hooks <bhooks1234@gmail.com>
2015-03-26 17:24:49 -0400

    Update PythonEasy.py

--- PythonEasy.py
+++ PythonEasy.py
@@ -1,9 +1,9 @@
 import time
-start = time.time()
+start = time.time()*1000
 for x in range(1,100):
         print x+x
         print x*x
         print x/x
         print x-x
 
-print time.time() - start
+print time.time()*1000 - start
